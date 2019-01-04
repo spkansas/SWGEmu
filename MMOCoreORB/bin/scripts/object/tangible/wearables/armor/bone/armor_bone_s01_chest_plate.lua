41335dc4ea233307a7a13e15e5b9c3011294f39d
AsaVentress <johnwalton763060@gmail.com>
2019-01-04 10:45:43 +0000

    [Added] Species to Original Armor & Aprons

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/bone/armor_bone_s01_chest_plate.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/bone/armor_bone_s01_chest_plate.lua
@@ -44,8 +44,27 @@
 object_tangible_wearables_armor_bone_armor_bone_s01_chest_plate = object_tangible_wearables_armor_bone_shared_armor_bone_s01_chest_plate:new {
 	templateType = ARMOROBJECT,
 
-	playerRaces = { "object/creature/player/bothan_male.iff",
+	playerRaces = { "object/creature/player/aqualish_male.iff",
+				"object/creature/player/bith_female.iff",
+				"object/creature/player/bith_male.iff",
+				"object/creature/player/chiss_female.iff",
+				"object/creature/player/chiss_male.iff",
+				"object/creature/player/devaronian_male.iff",
+				"object/creature/player/gotal_male.iff",
+				"object/creature/player/gran_male.iff",
+				"object/creature/player/hutt_female.iff",
+				"object/creature/player/hutt_male.iff",
+				"object/creature/player/ishi_tib_male.iff",
+				"object/creature/player/nautolan_male.iff",
+				"object/creature/player/nightsister_female.iff",
+				"object/creature/player/nikto_male.iff",
+				"object/creature/player/quarren_male.iff",
+				"object/creature/player/smc_female.iff",
+				"object/creature/player/talz_male.iff",
+				"object/creature/player/togruta_female.iff",
+				"object/creature/player/weequay_male.iff",
 				"object/creature/player/bothan_female.iff",
+				"object/creature/player/bothan_male.iff",
 				"object/creature/player/human_male.iff",
 				"object/creature/player/human_female.iff",
 				"object/creature/player/moncal_male.iff",
@@ -85,7 +104,7 @@ object_tangible_wearables_armor_bone_armor_bone_s01_chest_plate = object_tangibl
 				"object/mobile/vendor/twilek_male.iff",
 				"object/mobile/vendor/weequay_male.iff",
 				"object/mobile/vendor/zabrak_female.iff",
-				"object/mobile/vendor/zabrak_male.iff" },
+				"object/mobile/vendor/zabrak_male.iff", }
 
 	-- kineticeffectiveness, energyeffectiveness, electricaleffectiveness, stuneffectiveness, blasteffectiveness, heateffectiveness, coldeffectiveness, acideffectiveness, restraineffectiveness
 	vulnerability = KINETIC + BLAST + STUN + HEAT + COLD + ACID + LIGHTSABER,
